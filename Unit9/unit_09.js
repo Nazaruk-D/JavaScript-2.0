d984063fecfeb95fc4bd9adafdb343032b5ca532
Nazaruk-D <nazaruk-dima@mail.ru>
2022-06-01 15:50:39 +0300

    task 1-10

--- Unit9/unit_09.js
+++ Unit9/unit_09.js
@@ -66,6 +66,9 @@ document.querySelector('.b-5').onclick = f5;
 // По нажатию b-6 запускайте функцию f6, которая выводит в out-6 количество параграфов с классом p-6.
 
 function f6() {
+    let p = document.querySelectorAll ('.p-6');
+    let out = document.querySelector('.out-6');
+    out.innerHTML = p.length;
 
 }
 
@@ -75,9 +78,16 @@ document.querySelector('.b-6').onclick = f6;
 //  Task 7
 // По нажатию кнопки b-7 запускайте функцию f7, которая присваивает блокам out-7 класс .bg-7. Обратите внимание, что данных блоков больше одного, следовательно нужен цикл.
 
-let blocks7 = document.querySelectorAll('.out-7');
+// let blocks7 = document.querySelectorAll('.out-7');
 
 function f7() {
+    let num = 0;
+    let x = document.querySelectorAll('.out-7');
+    console.log(x.length);
+    while (num < x.length) {
+        x[num].classList.add ('bg-7');
+        num++;
+    }
     //внутри цикла blocks7[i].classList....
 }
 
@@ -87,9 +97,16 @@ document.querySelector('.b-7').onclick = f7;
 //  Task 8
 // По нажатию кнопки b-8 запускайте функцию f8, которая делает toggle блокам out-8 класс .bg-8. Обратите внимание, что данных блоков больше одного, следовательно нужен цикл.
 
-let blocks8 = document.querySelectorAll('.out-8');
+// let blocks8 = document.querySelectorAll('.out-8');
 
 function f8() {
+    let num = 0;
+    let x = document.querySelectorAll('.out-8');
+    console.log(x.length);
+    while (num < x.length) {
+        x[num].classList.toggle ('bg-8');
+        num++;
+    }
     //внутри цикла blocks8[i].classList....
 }
 
@@ -101,12 +118,11 @@ document.querySelector('.b-8').onclick = f8;
 
 function f9() {
     //this.classList...  // все решается одной строкой
+    this.classList.add ('bg-9');
 }
-
-let div9 = document.querySelectorAll('.out-9');
-
-for (let i = 0; i < div9.length; i++) {
-    div9[i].onclick = f9;
+let x = document.querySelectorAll('.out-9');
+for (let i = 0; i < x.length; i++) {
+    x[i].onclick = f9;
 }
 
 
@@ -114,13 +130,14 @@ for (let i = 0; i < div9.length; i++) {
 // Усложним предыдущие задачи. С помощью цикла повесьте на блоки out-10 событие клик. По клику должна выполняться функция f10. Функция, должна делать toggle класса bg-10 тому out-10 на котором кликнули.
 
 //let div10 = тут получите все out-10
-
+let x10 = document.querySelectorAll ('.out-10');
 function f10() {
-
+    this.classList.toggle ('bg-10');
 }
 
-// а тут цикл, похожий на предыдущее задание
-
+for (let i = 0; i < x10.length; i++) {
+    x10[i].onclick = f10;
+}
 
 
 //  Task 11
